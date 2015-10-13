6dbd43583bd6d896893f03bfeb9a408472a08cef
ericssoncamplus <zhipeng.kong@ericsson>
2015-10-13 17:49:30 +0800

    update height

--- js/game.js
+++ js/game.js
@@ -7,12 +7,17 @@ var game = function() {
         var leters = ['a', 'b', 'c', 'd', 'e'];
         var totrow = data.length;
         var totcol = data[0].length;
-        var html = '<div class="ui-grid-' + leters[totcol - 2] + ' ui-grid-height-' + leters[totcol - 2] + '">';
+        var html = '<div data-role="huangshouxi" class="ui-grid-' + leters[totcol - 2] + ' ui-grid-height-x ">';
+		//var $grid_width = $("div[data-role=huangshouxi]").width();
+		//var $window_width = $(window).width();		
+		//alert($grid_width);
         clearInterval(timout);
         $.each(data, function(i) {
             var self = this;
             $.each(self, function(j) {
                 html += '<div class="ui-block-' + leters[j % 5 ] + ' light">';
+				/*$grid_width = $(".light").width();
+				$grid_width = $grid_width + "px";*/
                 if (data[i][j] === 1) {
                     html += '<div class="ui-bar ui-bar-c"><img width="100%" height="100%"  src="css/images/ECON_white2.png"/></div>';
                 } else {
@@ -21,9 +26,26 @@ var game = function() {
                 html += '</div>';
             });
         });
-        html += '</div>';
+        html += '</div>';				
         $("#grid").html(html);
+		
+		//var $grid_width = $("div[data-role=huangshouxi]").width();
+		//var $grid_width = $("div[data-role=content]").width();
+		//var $grid_width = $("#grid").width();
+		//alert($grid_width);
+		
+		var $window_width = $(window).width();
+		var $each_grid_height = $window_width - 32;		
+		$each_grid_height = $each_grid_height + "px";
+		$("#grid").width("100%").height($each_grid_height);
+		
         $("#grid  div.light").on("click", function() {
+			//alert($grid_width);
+			//alert($window_width);
+			//alert($each_grid_height);
+			//alert($grid_width);
+			
+			
             var index = $(this).index();
             var col = index % totcol;
             var row = (index - col) / totcol;
@@ -62,9 +84,10 @@ var game = function() {
             }
             return true;
         }
-    }
+    }	
 
     return {
         init: init
-    }
+    }	
+	
 }();
